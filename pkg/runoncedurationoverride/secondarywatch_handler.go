9dd22b3731d93f5c84d27f8060a883d1cf657281
Jan Chaloupka <jchaloup@redhat.com>
2026-01-11 11:23:31 +0100

    refactor(pkg/runoncedurationoverride): use cache.ResourceEventHandlerFuncs for the secondary event handler

--- pkg/runoncedurationoverride/secondarywatch_handler.go
+++ pkg/runoncedurationoverride/secondarywatch_handler.go
@@ -7,63 +7,47 @@ import (
 	"github.com/openshift/run-once-duration-override-operator/pkg/runtime"
 )
 
-func newResourceEventHandler(enqueuer runtime.Enqueuer) *resourceEventHandler {
-	return &resourceEventHandler{
-		enqueuer: enqueuer,
-	}
-}
+func newResourceEventHandler(enqueuer runtime.Enqueuer) cache.ResourceEventHandler {
+	enqueueOwner := func(obj interface{}, context string) {
+		metaObj, err := runtime.GetMetaObject(obj)
+		if err != nil {
+			klog.Errorf("[secondarywatch] %s: invalid object, type=%T", context, obj)
+			return
+		}
 
-var _ cache.ResourceEventHandler = &resourceEventHandler{}
-
-type resourceEventHandler struct {
-	// When a secondary resource is updated we enqueue the owner (primary resource)
-	enqueuer runtime.Enqueuer
-}
-
-func (r *resourceEventHandler) OnAdd(obj interface{}, isInInitialList bool) {
-	metaObj, err := runtime.GetMetaObject(obj)
-	if err != nil {
-		klog.Errorf("[secondarywatch] OnAdd: invalid object, type=%T", obj)
-		return
+		if err := enqueuer.Enqueue(metaObj); err != nil {
+			klog.V(3).Infof("[secondarywatch] %s: failed to enqueue owner - %s", context, err.Error())
+		}
 	}
 
-	if err := r.enqueuer.Enqueue(metaObj); err != nil {
-		klog.V(3).Infof("[secondarywatch] OnAdd: failed to enqueue owner - %s", err.Error())
-	}
-}
+	return cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			enqueueOwner(obj, "OnAdd")
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			oldMetaObj, err := runtime.GetMetaObject(oldObj)
+			if err != nil {
+				klog.Errorf("[secondarywatch] OnUpdate: invalid object, type=%T", oldObj)
+				return
+			}
 
-// OnUpdate creates UpdateEvent and calls Update on EventHandler
-func (r *resourceEventHandler) OnUpdate(oldObj, newObj interface{}) {
-	oldMetaObj, err := runtime.GetMetaObject(oldObj)
-	if err != nil {
-		klog.Errorf("[secondarywatch] OnUpdate: invalid object, type=%T", oldObj)
-		return
-	}
+			newMetaObj, err := runtime.GetMetaObject(newObj)
+			if err != nil {
+				klog.Errorf("[secondarywatch] OnUpdate: invalid object, type=%T", newObj)
+				return
+			}
 
-	newMetaObj, err := runtime.GetMetaObject(newObj)
-	if err != nil {
-		klog.Errorf("[secondarywatch] OnUpdate: invalid object, type=%T", newObj)
-		return
-	}
+			if oldMetaObj.GetResourceVersion() == newMetaObj.GetResourceVersion() {
+				klog.V(3).Infof("[secondarywatch] OnUpdate: resource version has not changed, not going to enqueue owner, type=%T resource-version=%s", newMetaObj, newMetaObj.GetResourceVersion())
+				return
+			}
 
-	if oldMetaObj.GetResourceVersion() == newMetaObj.GetResourceVersion() {
-		klog.V(3).Infof("[secondarywatch] OnUpdate: resource version has not changed, not going to enqueue owner, type=%T resource-version=%s", newMetaObj, newMetaObj.GetResourceVersion())
-		return
-	}
-
-	if err := r.enqueuer.Enqueue(newMetaObj); err != nil {
-		klog.V(3).Infof("[secondarywatch] OnUpdate: failed to enqueue owner - %s", err.Error())
-	}
-}
-
-func (r *resourceEventHandler) OnDelete(obj interface{}) {
-	metaObj, err := runtime.GetMetaObject(obj)
-	if err != nil {
-		klog.Errorf("[secondarywatch] OnDelete: invalid object, type=%T", obj)
-		return
-	}
-
-	if err := r.enqueuer.Enqueue(metaObj); err != nil {
-		klog.V(3).Infof("[secondarywatch] OnDelete: failed to enqueue owner - %s", err.Error())
+			if err := enqueuer.Enqueue(newMetaObj); err != nil {
+				klog.V(3).Infof("[secondarywatch] OnUpdate: failed to enqueue owner - %s", err.Error())
+			}
+		},
+		DeleteFunc: func(obj interface{}) {
+			enqueueOwner(obj, "OnDelete")
+		},
 	}
 }
