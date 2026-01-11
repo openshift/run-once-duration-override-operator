687001d06ac38cf652caa858809a632a7af2b4cb
Jan Chaloupka <jchaloup@redhat.com>
2026-01-11 12:43:41 +0100

    refactor(pkg/runoncedurationoverride): rearrange controller.go's New function statements

--- pkg/runoncedurationoverride/controller.go
+++ pkg/runoncedurationoverride/controller.go
@@ -36,6 +36,9 @@ func New(options *Options) (c Interface, err error) {
 		return
 	}
 
+	// setup operand asset
+	operandAsset := asset.New(options.RuntimeContext)
+
 	// create lister(s) for secondary resources
 	deployment := options.InformerFactory.Apps().V1().Deployments()
 	daemonset := options.InformerFactory.Apps().V1().DaemonSets()
@@ -45,6 +48,8 @@ func New(options *Options) (c Interface, err error) {
 	secret := options.InformerFactory.Core().V1().Secrets()
 	serviceaccount := options.InformerFactory.Core().V1().ServiceAccounts()
 	webhook := options.InformerFactory.Admissionregistration().V1().MutatingWebhookConfigurations()
+	// Create RunOnceDurationOverride informer using the standard informer factory
+	rodooInformer := options.OperatorInformerFactory.RunOnceDurationOverride().V1().RunOnceDurationOverrides()
 
 	secondaryLister := &SecondaryLister{
 		deployment:     deployment.Lister(),
@@ -57,43 +62,16 @@ func New(options *Options) (c Interface, err error) {
 		webhook:        webhook.Lister(),
 	}
 
+	lister := rodooInformer.Lister()
+
 	// We need a queue
 	queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
-
-	// Create RunOnceDurationOverride informer using the standard informer factory
-	rodooInformer := options.OperatorInformerFactory.RunOnceDurationOverride().V1().RunOnceDurationOverrides()
 
 	// Add event handler to the informer
 	_, err = rodooInformer.Informer().AddEventHandler(newEventHandler(queue))
 	if err != nil {
 		return
 	}
-
-	lister := rodooInformer.Lister()
-	informer := rodooInformer.Informer()
-
-	// setup operand asset
-	operandAsset := asset.New(options.RuntimeContext)
-
-	r := NewReconciler(&HandlerOptions{
-		OperandContext:  options.RuntimeContext,
-		Client:          options.Client,
-		PrimaryLister:   lister,
-		SecondaryLister: secondaryLister,
-		Asset:           operandAsset,
-		Recorder:        options.Recorder,
-	})
-
-	c = &runOnceDurationOverrideController{
-		workers:    options.Workers,
-		queue:      queue,
-		informer:   informer,
-		reconciler: r,
-		lister:     lister,
-	}
-
-	// setup watches for secondary resources
-	handler := newResourceEventHandler(queue, lister, operandAsset.Values().OwnerAnnotationKey)
 
 	informers := []cache.SharedIndexInformer{
 		deployment.Informer(),
@@ -107,10 +85,28 @@ func New(options *Options) (c Interface, err error) {
 	}
 
 	for _, informer := range informers {
-		_, err = informer.AddEventHandler(handler)
+		// setup watches for secondary resources
+		_, err = informer.AddEventHandler(newResourceEventHandler(queue, lister, operandAsset.Values().OwnerAnnotationKey))
 		if err != nil {
 			return
 		}
+	}
+
+	r := NewReconciler(&HandlerOptions{
+		OperandContext:  options.RuntimeContext,
+		Client:          options.Client,
+		PrimaryLister:   lister,
+		SecondaryLister: secondaryLister,
+		Asset:           operandAsset,
+		Recorder:        options.Recorder,
+	})
+
+	c = &runOnceDurationOverrideController{
+		workers:    options.Workers,
+		queue:      queue,
+		informer:   rodooInformer.Informer(),
+		reconciler: r,
+		lister:     lister,
 	}
 
 	return
