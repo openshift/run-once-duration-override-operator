fe34320fe81cad2e934a14768f7697d6bdf6bdae
Jan Chaloupka <jchaloup@redhat.com>
2026-01-12 12:15:10 +0100

    refactor(pkg/runoncedurationoverride.newResourceEventHandler): use a shared function to detect an ownership

--- pkg/runoncedurationoverride/handler.go
+++ pkg/runoncedurationoverride/handler.go
@@ -8,24 +8,43 @@ import (
 	"k8s.io/klog/v2"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
+	"github.com/openshift/library-go/pkg/controller/factory"
 	runoncedurationoverridev1 "github.com/openshift/run-once-duration-override-operator/pkg/apis/runoncedurationoverride/v1"
 	"github.com/openshift/run-once-duration-override-operator/pkg/operator/operatorclient"
 	"github.com/openshift/run-once-duration-override-operator/pkg/runtime"
 )
 
+func isOwnedByOperator(obj interface{}) bool {
+	// Check if obj is a RunOnceDurationOverride
+	if rodoo, ok := obj.(*runoncedurationoverridev1.RunOnceDurationOverride); ok {
+		return rodoo.Name == operatorclient.OperatorConfigName
+	}
+
+	// For other types, check if they are owned by the operator
+	metaObj, err := runtime.GetMetaObject(obj)
+	if err != nil {
+		klog.V(4).Infof("failed to get meta object: %v", err)
+		return false
+	}
+
+	ownerName := getOwnerName(metaObj)
+	return ownerName == operatorclient.OperatorConfigName
+}
+
+var _ factory.EventFilterFunc = isOwnedByOperator
+
 // newEventHandler returns a cache.ResourceEventHandler appropriate for
 // reconciliation of RunOnceDurationOverride object(s).
 func newEventHandler(queue workqueue.RateLimitingInterface) cache.ResourceEventHandler {
-	enqueue := func(key string) {
-		namespace, name, err := cache.SplitMetaNamespaceKey(key)
-		if err != nil {
+	enqueue := func(obj interface{}) {
+		if !isOwnedByOperator(obj) {
 			return
 		}
 
 		request := reconcile.Request{
 			NamespacedName: types.NamespacedName{
-				Namespace: namespace,
-				Name:      name,
+				Namespace: "",
+				Name:      operatorclient.OperatorConfigName,
 			},
 		}
 
@@ -33,12 +52,12 @@ func newEventHandler(queue workqueue.RateLimitingInterface) cache.ResourceEventH
 	}
 
 	add := func(obj interface{}) {
-		key, err := cache.MetaNamespaceKeyFunc(obj)
+		_, err := cache.MetaNamespaceKeyFunc(obj)
 		if err != nil {
 			klog.Errorf("could not extract key, type=%T", obj)
 			return
 		}
-		enqueue(key)
+		enqueue(obj)
 	}
 
 	return cache.ResourceEventHandlerFuncs{
@@ -50,11 +69,11 @@ func newEventHandler(queue workqueue.RateLimitingInterface) cache.ResourceEventH
 			add(newObj)
 		},
 		DeleteFunc: func(obj interface{}) {
-			key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+			_, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
 			if err != nil {
 				return
 			}
-			enqueue(key)
+			enqueue(obj)
 		},
 	}
 }
@@ -79,15 +98,7 @@ func getOwnerName(object metav1.Object) string {
 
 func newResourceEventHandler(queue workqueue.RateLimitingInterface) cache.ResourceEventHandler {
 	enqueueOwner := func(obj interface{}, context string) {
-		metaObj, err := runtime.GetMetaObject(obj)
-		if err != nil {
-			klog.Errorf("[secondarywatch] %s: invalid object, type=%T", context, obj)
-			return
-		}
-
-		ownerName := getOwnerName(metaObj)
-		if ownerName == "" {
-			klog.V(3).Infof("[secondarywatch] %s: could not find owner for %s/%s", context, metaObj.GetNamespace(), metaObj.GetName())
+		if !isOwnedByOperator(obj) {
 			return
 		}
 
